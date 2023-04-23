f1ccc7e845576bae857843874affc6419bb8318d
Jason <68597004+jaybay10@users.noreply.github.com>
2023-04-23 14:56:13 +0200

    foundation of project setup completed

--- project-app/src/app/app.module.ts
+++ project-app/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TaskHeaderComponent } from './task-header/task-header.component';
+import { TaskListsComponent } from './task-lists/task-lists.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskHeaderComponent,
+    TaskListsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project-app/src/app/task-lists/task-lists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-lists',
+  templateUrl: './task-lists.component.html',
+  styleUrls: ['./task-lists.component.css']
+})
+export class TaskListsComponent {
+
+}
